e5fa5c1aea197ac28d77c75d4184b61719f61258
hamed alizadegan <hamed.alizadegan@gmail.com>
2024-04-15 19:20:15 +0330

    update

--- journal/serializers.py
+++ journal/serializers.py
@@ -11,8 +11,7 @@ class JournalSerializer(serializers.ModelSerializer):
 
     def get_tag(self, obj):
         title_journal = obj
-        print(type(title_journal))
         # journal = JournalModel.objects.get(title=title_journal)
         # tags = journal.journal_tag.all()
         # tag_list = [tag.tag.tag for tag in tags]
-        return 'tag_list'
+        return type(title_journal)
