f40f9d0d5e713f539e9e57a33ab0555c70907d9f
hamed alizadegan <hamed.alizadegan@gmail.com>
2024-04-15 19:43:08 +0330

    update

--- journal/admin.py
+++ journal/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
-from .models import JournalModel
+from .models import JournalModel, AddTagModel
+
+
+class TagAdmin(admin.ModelAdmin):
+    readonly_fields = ["slug"]
+
+
+class TagInline(admin.TabularInline):
+    model = AddTagModel
 
 
 class JournalAdmin(admin.ModelAdmin):
     readonly_fields = ["slug"]
+    inlines = (TagInline,)
 
 
 admin.site.register(JournalModel, JournalAdmin)
-
-
+admin.site.register(AddTagModel, TagAdmin)
