abb7971493149275c8f0c7d4f46feeed03bdd437
hamed alizadegan <hamed.alizadegan@gmail.com>
2024-04-15 19:01:44 +0330

    update

--- journal/serializers.py
+++ journal/serializers.py
@@ -3,6 +3,15 @@ from .models import JournalModel
 
 
 class JournalSerializer(serializers.ModelSerializer):
+    tag = serializers.SerializerMethodField()
+
     class Meta:
         model = JournalModel
         fields = "__all__"
+
+    def get_tag(self, obj):
+        # product_name = obj
+        # product = ProductModel.objects.get(product=product_name)
+        # subcategories = product.subcategory_product.all()
+        # subcategory_list = [subcategory.subcategory.subcategory for subcategory in subcategories]
+        return 'tag'
