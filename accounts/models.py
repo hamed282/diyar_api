9835ce131998ec10eed144781687d2aaa5064bda
hamed alizadegan <hamed.alizadegan@gmail.com>
2024-05-21 15:09:19 +0330

    update

--- accounts/models.py
+++ accounts/models.py
@@ -22,11 +22,11 @@ class User(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return f'{self.first_name} - {self.last_name}'
 
-    def has_perm(self, perm, obj=None):
-        return True
-
-    def has_module_perms(self, app_label):
-        return True
+    # def has_perm(self, perm, obj=None):
+    #     return True
+    #
+    # def has_module_perms(self, app_label):
+    #     return True
 
     @property
     def is_staff(self):
